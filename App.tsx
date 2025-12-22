32081ffeccadfb2fe0ecc2d592bc75cece8e1b86
CyberJonah <abwajona21@gmail.com>
2025-12-22 23:08:38 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SplashScreen from "./screens/SplashScreen";
+import AuthScreen from "./screens/AuthScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Auth" component={AuthScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
